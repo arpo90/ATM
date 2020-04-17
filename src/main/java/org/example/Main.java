ce34256684b6cc78c88c1fa0c9dc27461fe3b030
denisrodin <denis.for.logs@gmail.com>
2020-04-17 09:02:42 +0300

    добавлены классы серверной и клиентской части

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,29 @@
 package org.example;
 
-import java.io.IOException;
+import org.example.server.product.Account;
+import org.example.server.product.Balance;
+import org.example.server.product.Card;
+import org.example.server.Host;
+
 import java.time.LocalDate;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ATM atm = new ATM();
+        Host host = new Host(
+                        new Card(
+                                new Account(
+                                        new Balance(
+                                                1
+                                                ,"USD"
+                                        )
+                                )
+                                , LocalDate.now()
+                                ,1231111111
+                                ,123
+                        )
+                    );
 
-        atm.readCard(LocalDate.of(2021, 5, 1), "1234 1234 1234 1234");
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите PIN:");
-
-        int pin = sc.nextInt();
-        try {
-            atm.readPin(pin);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return;
-        }
-
-        String balance = atm.getBalance();
-        System.out.println(balance);
+        System.out.println(host);
     }
 }
